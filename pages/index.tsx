a186c8be046c633347d41f948333d6e2ae2dd51e
Adrian Ardizza <adrian.ardizza@gmail.com>
2021-09-10 20:45:56 +0700

    Added Lab1 testcases

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,9 @@ const Editor = dynamic(import("../components/Editor"), {
 const Status: React.FC<{ value: string }> = ({ value }) => {
   return (
     <span
-      className={`${(value === "WA" || value === "TLE") && "text-red-500"}`}
+      className={`${(value === "WA" || value === "TLE") && "text-red-500"} ${
+        value === "AC" && "text-green-500"
+      }`}
     >
       {value}
     </span>
@@ -32,6 +34,9 @@ const Home: NextPage = () => {
   const [currentTestCase, setCurrentTestCase] = React.useState<any>();
   const [currentProblemSet, setCurrentProblemSet] = React.useState<any>("");
 
+  const [input, setInput] = React.useState("");
+  const [output, setOutput] = React.useState("");
+
   React.useEffect(() => {
     fetch(`${baseUrl}/index.txt`)
       .then((res) => res.text())
@@ -42,26 +47,43 @@ const Home: NextPage = () => {
     if (currentProblemSet) {
       fetch(`${baseUrl}/${currentProblemSet}/num.txt`, { cache: "no-cache" })
         .then((res) => res.text())
-        .then(
-          (item: string) =>
-            // @ts-ignore
-            !isNaN(item) &&
+        .then((item: string) => {
+          // @ts-ignore
+          !isNaN(item) &&
             setTestCases(
               [...new Array(Number(item))].map(
                 (item: any, index: number) => index + 1
               )
-            )
-        );
+            );
+          setCurrentTestCase("");
+        });
     }
   }, [currentProblemSet]);
 
+  React.useEffect(() => {
+    if (currentProblemSet && currentTestCase) {
+      fetch(`${baseUrl}/${currentProblemSet}/in${currentTestCase}.txt`, {
+        cache: "no-cache",
+      })
+        .then((res) => res.text())
+        .then((text) => setInput(text));
+
+      fetch(`${baseUrl}/${currentProblemSet}/output${currentTestCase}.txt`, {
+        cache: "no-cache",
+      })
+        .then((res) => res.text())
+        .then((text) => setOutput(text));
+    }
+  }, [currentTestCase]);
+
   const onSubmit = () => {
+    setResult({});
     fetch("http://localhost:3006/code/submit", {
       method: "POST",
       body: JSON.stringify({
         code,
-        input: "",
-        output: "",
+        input,
+        output,
       }),
       headers: new Headers({
         "Content-Type": "application/json",
@@ -72,7 +94,7 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center bg-gray-800 min-h-screen w-full h-full">
+    <div className="flex flex-col justify-center bg-gray-800 min-h-screen w-full h-full py-16">
       <div className="w-full mx-auto max-w-5xl">
         <h1 className="text-white font-bold mb-2">Enter your code here</h1>
         <Editor
@@ -81,15 +103,9 @@ const Home: NextPage = () => {
           onChange={(code) => setCode(code)}
         />
       </div>
-      <div className="w-full mx-auto max-w-5xl mt-2 flex justify-between mb-2">
-        <p className="text-white font-bold">
-          Status: <Status value={result?.message} />
-        </p>
-        <p className="text-white font-bold">Time: {result?.output?.time}ms</p>
-      </div>
-      <div className="w-full mx-auto max-w-5xl mb-4 flex justify-between">
+      <div className="w-full mx-auto max-w-5xl mb-4 flex justify-between mt-2">
         <div className="w-64">
-          <h1 className="text-white font-bold mb-2">Test Case</h1>
+          <h1 className="text-white font-bold mb-2">Problem Set</h1>
           <Select
             placeholder="Select a problem set."
             size="md"
@@ -104,12 +120,13 @@ const Home: NextPage = () => {
           </Select>
         </div>
         <div className="w-64">
-          <h1 className="text-white font-bold mb-2">Problem Set</h1>
+          <h1 className="text-white font-bold mb-2">Test Case</h1>
           <Select
             placeholder="Select a testcase."
             size="md"
-            variant="outline bg-white"
+            variant="outline bg-gray-500"
             onChange={(e) => setCurrentTestCase(e.target.value)}
+            value={currentTestCase}
           >
             {testCases.map((item: string) => (
               <option key={item} value={item}>
@@ -125,6 +142,8 @@ const Home: NextPage = () => {
           <Editor
             className="overflow-auto"
             style={{ width: "100%", maxHeight: "16rem" }}
+            value={input}
+            onChange={(e) => setInput(e)}
           />
         </div>
         <div>
@@ -136,7 +155,13 @@ const Home: NextPage = () => {
           />
         </div>
       </div>
-      <div className="mt-2 max-w-5xl w-full mx-auto flex justify-center">
+      <div className="w-full mx-auto max-w-5xl mt-2 flex justify-between mb-2">
+        <p className="text-white font-bold">
+          Status: <Status value={result?.message} />
+        </p>
+        <p className="text-white font-bold">Time: {result?.output?.time}ms</p>
+      </div>
+      <div className="mt-2 max-w-5xl w-full mx-auto flex justify-end">
         <Button onClick={onSubmit}>Submit</Button>
       </div>
     </div>
