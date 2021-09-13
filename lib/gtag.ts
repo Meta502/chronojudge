11db1e404369fbc4e8742c0d566cece2d35e3359
Adrian Ardizza <adrian.ardizza@gmail.com>
2021-09-14 02:03:58 +0700

    Implement google analytics

---
+++ lib/gtag.ts
@@ -0,0 +1,25 @@
+export const GA_TRACKING_ID = "G-2LCSFP2YP0";
+
+export const pageview = (url: URL): void => {
+  // @ts-ignore
+  window.gtag("config", GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+type GTagEvent = {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: GTagEvent): void => {
+  // @ts-ignore
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value,
+  });
+};
