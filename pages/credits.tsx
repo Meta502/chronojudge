9d39c6459474c49d2c5d3a36325dc8e48f7f14bc
Adrian Ardizza <adrian.ardizza@gmail.com>
2021-10-06 12:32:37 +0700

    Added donator names to website

--- pages/credits.tsx
+++ pages/credits.tsx
@@ -4,6 +4,62 @@ import Header from "../components/General/Header";
 import Link from "../components/General/Link";
 import Footer from "../components/General/Footer";
 
+const donators = [
+  {
+    name: "vincent s",
+    amount: 50000,
+  },
+  {
+    name: "Dixon",
+    amount: 25000,
+  },
+  {
+    name: "bakastev",
+    amount: 10000,
+  },
+  {
+    name: "Gaji asdos yang tak dibayar2",
+    amount: 50000,
+  },
+  {
+    name: "hy landy",
+    amount: 25000,
+  },
+  {
+    name: "Tito",
+    amount: 50000,
+  },
+  {
+    name: "algo ku N(O!)",
+    amount: 20659,
+  },
+  {
+    name: "Kevin",
+    amount: 10000,
+  },
+  {
+    name: "Ndar",
+    amount: 25000,
+  },
+  {
+    name: "Rasengaaaan",
+    amount: 50000,
+  },
+  {
+    name: "larrypult",
+    amount: 100000,
+  },
+  {
+    name: "Mymy",
+    amount: 20000,
+  },
+];
+
+const formatter = new Intl.NumberFormat("id-ID", {
+  style: "currency",
+  currency: "IDR",
+});
+
 const Credits: React.FC = () => {
   return (
     <>
@@ -42,6 +98,33 @@ const Credits: React.FC = () => {
               </Link>{" "}
               (Kak Firdaus Al-Ghifari).
             </p>
+          </div>
+        </div>
+        <div
+          className="max-w-5xl mx-auto w-full p-6 rounded-xl space-y-4"
+          style={{ backgroundColor: "#222222" }}
+        >
+          <div>
+            <h1 className="font-bold">Server Hosting Donators:</h1>
+            <div className="grid grid-cols-3 gap-4 mt-4">
+              {donators
+                .sort((a, b) => b.amount - a.amount)
+                .map((item) => {
+                  return (
+                    <div
+                      key={item.name}
+                      style={{ backgroundColor: "#303030" }}
+                      className="p-4 rounded-lg"
+                    >
+                      <h1 className="font-semibold text-lg ">{item.name}</h1>
+                      <p>
+                        <span className="font-semibold">Amount:</span>{" "}
+                        {formatter.format(item.amount)}
+                      </p>
+                    </div>
+                  );
+                })}
+            </div>
           </div>
         </div>
       </div>
