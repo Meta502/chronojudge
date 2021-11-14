be87d8e78b71be8d6cc190a255be659e46f3f324
Adrian Ardizza <adrian.ardizza@gmail.com>
2021-11-14 16:27:31 +0700

    Add link to HzzGrader

--- components/General/Header.tsx
+++ components/General/Header.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Chronos from "./Chronos";
+import Link from "./Link";
 
 const Header = () => (
-  <div className="w-full mx-auto max-w-5xl flex items-center justify-center mb-4">
-    <Chronos />
-    <h1 className="font-bold text-2xl ml-4">ChronoJudge</h1>
-  </div>
+  <>
+    <div className="w-full mx-auto max-w-5xl flex items-center justify-center mb-4">
+      <Chronos />
+      <h1 className="font-bold text-2xl ml-4">ChronoJudge</h1>
+    </div>
+    <p className="mx-auto font-semibold text-center">
+      Check out this program-based grader by HzzkyGcs (better for running large
+      testcases)
+      <br />
+      <Link target="_blank" href="https://github.com/Hzzkygcs/SDA/releases/">
+        HzzGrader
+      </Link>
+    </p>
+  </>
 );
 
 export default Header;
